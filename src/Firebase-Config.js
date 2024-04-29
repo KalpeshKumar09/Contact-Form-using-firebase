c18a660d4cff129dccb5e64285f565030c3c6291
KalpeshSuthar09 <sutharkalpesh101@gmail.com>
2024-04-29 16:26:23 +0530

    first commit

---
+++ src/Firebase-Config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBb6pz99LCiHZQu5CCK3vZ51XvjT--z2v4",
+  authDomain: "contact-form-1a11d.firebaseapp.com",
+  projectId: "contact-form-1a11d",
+  storageBucket: "contact-form-1a11d.appspot.com",
+  messagingSenderId: "136671704136",
+  appId: "1:136671704136:web:56b471c0971577815d547d",
+  measurementId: "G-RQYQGKNVDN",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
