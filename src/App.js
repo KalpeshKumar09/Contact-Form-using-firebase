d27a10cebcf37c0a81edd89157dae34cc996d333
KalpeshSuthar09 <sutharkalpesh101@gmail.com>
2024-04-30 22:45:39 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./App.css";
-import Contact from "./component/Contact/Contact";
+import Form from "./component/ProfileForm/Form";
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <Form />
     </div>
   );
 }
